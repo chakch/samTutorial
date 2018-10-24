3dfefedf4b9579490fa3bbd71ac8dfe6402cf3d5
mchaaben <mchaaben@xebia.fr>
2018-10-24 20:27:30 +0200

    migrate to sam ok

---
+++ audio_transcoder/S3.js
@@ -0,0 +1,17 @@
+const AWS = require('aws-sdk');
+
+
+const s3 ={};
+const saveFile = (file) => {
+    const S3 = new AWS.S3();
+    let params = {
+        Bucket : "serverless-xke-demo",
+        Key : "key.mp3",
+        Body : file,
+        ContentType:'audio/mp3'
+    };
+    return S3.upload(params).promise();
+};
+
+s3.saveFile = saveFile;
+module.exports = s3;
---
+++ rss_reader/model/article.js
@@ -0,0 +1,18 @@
+const vogels = require('../config/vogels');
+const Joi = require('joi');
+
+const Article = vogels.define('article', {
+    hashKey: 'id',
+    timestamps: true,
+    schema: {
+        id: Joi.string(),
+        description: Joi.string(),
+        title: Joi.string(),
+        link: Joi.string(),
+        date: Joi.date()
+    },
+    tableName: 'article'
+});
+
+
+module.exports = Article;
